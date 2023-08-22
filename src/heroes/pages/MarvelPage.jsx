1c22f3d3149ac72aecfa0b414ea2189b28929750
Denny Edwin Huaman Mayta <cyberamor_angel@hotmail.com>
2023-08-21 22:46:41 -0500

    Cambios Marvel Page

--- src/heroes/pages/MarvelPage.jsx
+++ src/heroes/pages/MarvelPage.jsx
@@ -3,7 +3,7 @@ import { HeroList } from "../components/HeroList"
 export const MarvelPage = () => {
   return (
     <>
-      <h1>Marvel Comics</h1>
+      <h1>Marvel Comics !!!</h1>
       <hr />
 
       <HeroList publisher={ 'Marvel Comics' } />
